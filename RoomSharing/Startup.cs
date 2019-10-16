c51c04819a014955c0b34d5d39f1a7ac65388c50
Arockia Magimai Nathan J <arockiaraj@pdinet.com>
2019-10-16 21:45:50 +0530

    Add project files.

---
+++ RoomSharing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoomSharing.Startup))]
+namespace RoomSharing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
